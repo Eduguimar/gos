86776a5deec90b6cad90521e57a5655d83a01447
Eduguimara <eduguimara@gmail.com>
2026-07-25 15:53:25 -0300

    Initial commit

---
+++ src/test/java/com/devedu/gos/GosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devedu.gos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
